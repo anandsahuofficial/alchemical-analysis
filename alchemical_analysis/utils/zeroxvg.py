3062efbc290c651398fc26c24db90ccb26abe70d
Anand Sahu <anandsahuofficial@gmail.com>
2024-05-24 17:38:47 -0400

    changed py2 to py3

--- alchemical_analysis/utils/zeroxvg.py
+++ alchemical_analysis/utils/zeroxvg.py
@@ -2,7 +2,7 @@ import numpy,os,sys
 
 def zero_output(K,P):
    """Adapted from totalEnergies() function to write out all zeros in results.txt file"""
-   print "Generating results.txt with all 0"
+   print("Generating results.txt with all 0")
    df_allk = list(); ddf_allk = list()
    for k in range(K-1):
       df=dict(); ddf=dict()
@@ -50,7 +50,7 @@ def zero_output(K,P):
 
 def zero_dFt(K,P,nsnapshots):
 
-   print "Generating dF_t.txt with all 0"
+   print("Generating dF_t.txt with all 0")
 
    # Define a list of bForwrev equidistant time frames at which the free energy is to be estimated; count up the snapshots embounded between the time frames.
    n_tf = P.bForwrev + 1
@@ -79,4 +79,5 @@ def zero_dFt(K,P,nsnapshots):
    outtext = ["%12s %10s %-10s %17s %10s %s\n" % ('Time (ps)', 'Forward', P.units, 'Time (ps)', 'Reverse', P.units)]
    outtext+= ["%10s %11.3f +- %5.3f %18s %11.3f +- %5.3f\n" % (ts[1:][i], F_df[i], F_ddf[i], ts[:-1][i], R_df[i], R_ddf[i]) for i in range(len(F_df))]
    outfile = open(os.path.join(P.output_directory, 'dF_t.txt'), 'w'); outfile.writelines(outtext); outfile.close()
+
 
